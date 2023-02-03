393d1a64cfe508729aeb0a57ef282a2188239c54
itstimi-XD <ssk0710402@gmail.com>
2023-02-03 21:29:09 +0900

    #9 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 초기 기술
    *스프링 웹
    *엑츄에이터
    *데브툴즈
    *롬북

---
+++ toyproject2-board/src/main/java/fastcampus/toyproject2board/Toyproject2BoardApplication.java
@@ -0,0 +1,13 @@
+package fastcampus.toyproject2board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toyproject2BoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Toyproject2BoardApplication.class, args);
+    }
+
+}
---
+++ toyproject2-board/src/test/java/fastcampus/toyproject2board/Toyproject2BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package fastcampus.toyproject2board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Toyproject2BoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
