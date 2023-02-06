d42bf2262772d80127cc50f3e927573e3516048b
gh999 <gh934@naver.com>
2023-02-06 15:32:32 +0900

    feat: domain 설계

--- toyproject2-board/src/main/java/FQAboard/domain/FQAboard.java
+++ toyproject2-board/src/main/java/FQAboard/domain/FQAboard.java
@@ -1,7 +1,25 @@
 package FQAboard.domain;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
-public class FQAboard {
+public class FQAboard extends FAQBaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    @Column(length = 100,nullable = false)
+    private String FAQtype;
+
+    @Column(length = 250,nullable = false)
+    private String title;
+
+    @Column(length = 5000)
+    private String content;
+
+    @Column(length = 250,nullable = false)
+    private String id;
+
+
 }
