673f862205c1260073291feb792f596b51f5dd25
itstimi-XD <ssk0710402@gmail.com>
2023-02-07 15:23:41 +0900

    #10 - 도메인 클래스 설계(자유게시판, 댓글)
    도메인 정보를 설계해 보았다.
    기초적인 게시글과 댓글 표현이 가능하도록 함
    DB와 연동을 위한 추가적인 표현은 추후에 업데이트 될 수 있음.

---
+++ toyproject2-board/src/main/java/fastcampus/toyproject2board/domain/FreeArticleComment.java
@@ -0,0 +1,14 @@
+package fastcampus.toyproject2board.domain;
+
+import java.time.LocalDateTime;
+
+public class FreeArticleComment {
+    private Long id;
+    private FreeArticle freeArticle; //게시글 (id)
+    private String content;//본문
+
+    private LocalDateTime createdAt;//생성일시
+    private String createdBy;//생성자
+    private LocalDateTime modifiedAt;//수정일시
+    private String modifiedBy;//수정자
+}
