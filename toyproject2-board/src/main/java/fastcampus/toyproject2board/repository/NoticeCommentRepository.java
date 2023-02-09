e14c921b42995c3049862bc7a8cc2944f7f4c300
Chopaky <hommetuna@gmail.com>
2023-02-10 02:27:34 +0900

    [박동원] Spring Data Rest + HAL Explorer 디펜던시 추가
    API 간소화를 위해 스프링 부트의 기능을 추가했습니다.

--- toyproject2-board/src/main/java/fastcampus/toyproject2board/repository/NoticeCommentRepository.java
+++ toyproject2-board/src/main/java/fastcampus/toyproject2board/repository/NoticeCommentRepository.java
@@ -2,6 +2,8 @@ package fastcampus.toyproject2board.repository;
 
 import fastcampus.toyproject2board.domain.NoticeComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface NoticeCommentRepository extends JpaRepository<NoticeComment, Long> {
 }
--- toyproject2-board/src/main/java/fastcampus/toyproject2board/repository/NoticeRepository.java
+++ toyproject2-board/src/main/java/fastcampus/toyproject2board/repository/NoticeRepository.java
@@ -2,6 +2,9 @@ package fastcampus.toyproject2board.repository;
 
 import fastcampus.toyproject2board.domain.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
 }
