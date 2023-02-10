7a060c11abe7d9b7ab12dbe3c3ac8ca16ff96ed4
itstimi-XD <ssk0710402@gmail.com>
2023-02-10 15:30:57 +0900

    [김인후] - data rest 설정
    게시글, 댓글의 json api 를 자동으로
    restful 하게 만들게끔 설정

--- toyproject2-board/src/main/java/fastcampus/toyproject2board/repository/ArticleRepository.java
+++ toyproject2-board/src/main/java/fastcampus/toyproject2board/repository/ArticleRepository.java
@@ -2,6 +2,10 @@ package fastcampus.toyproject2board.repository;
 
 import fastcampus.toyproject2board.freeboard.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+
+@RepositoryRestResource
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
