b1c678429c9ce57f9980d97cfe75f0659f0c5c39
Chopaky <hommetuna@gmail.com>
2023-02-10 13:46:40 +0900

    [박동원] 게시글 뷰 골격 제작하였습니다.

--- toyproject2-board/src/main/java/fastcampus/toyproject2board/controller/NoticeController.java
+++ toyproject2-board/src/main/java/fastcampus/toyproject2board/controller/NoticeController.java
@@ -3,6 +3,7 @@ package fastcampus.toyproject2board.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,5 +16,12 @@ public class NoticeController {
     public String notices(ModelMap map){
         map.addAttribute("notices", List.of());
         return "notices/index";
+    }
+
+    @GetMapping("/{noticeId}")
+    public String notice(@PathVariable Long noticeId, ModelMap map){
+        map.addAttribute("notices", null);
+        map.addAttribute("noticesComments", List.of());
+        return "notices/detail";
     }
 }
